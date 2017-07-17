f091f4701ad13bf902531c4c405430a492fe0184
holu15795 <holu15795@gmail.com>
2017-07-17 15:30:00 +0700

    them chú thích

--- src/pages/lichcongviec/lichcongviec.ts
+++ src/pages/lichcongviec/lichcongviec.ts
@@ -82,7 +82,7 @@ export class LichCongViecPage {
     GioCv; PhutCv;
     NgayCv; ThangCv;
     TrangThai = 0;
-    Checked = false;
+    // Checked = false;
     idNotify;
     checkTat = false;
     checkBat = false;
@@ -110,9 +110,9 @@ export class LichCongViecPage {
                 this.ThoiGianLap = 5;
             }
         });
-        this.Lich.forEach(element => {
-            element["TrangThai"] = 0;
-        });
+        // this.Lich.forEach(element => {
+        //     element["TrangThai"] = 0;
+        // });
     }
     ionViewDidLoad() {
         // this.TonTai();
@@ -123,6 +123,7 @@ export class LichCongViecPage {
             console.log("Canncel all:");
             console.log(results);
         });
+        // xóa tất cả notify đã thông báo.
         LocalNotifications.clearAll().then(results => {
             console.log("Clear all:");
             console.log(results);
@@ -130,11 +131,14 @@ export class LichCongViecPage {
         this.HienThongBao("Thông Báo", "Xóa All");
         console.log("Xóa All");
     }
+    //even khi nhấn giữ 1 công việc để đặt hay tắt thông báo cho công việc đó
     showSheet(id) {
         //cộng id thêm 1 dãy để tạo id notify khác nhau của id đó.
         this.idNotify = id + (15071995 * 1);
+        //kiểm tra xem id này đã có thông báo chưa
         this.KiemTra(this.idNotify);
         let actionSheet;
+        //lấy id của công việc đang chọn set vào Detail để sử dụng.
         this.Lich.forEach(element => {
             if (element.id == id) {
                 this.Detail.id = element.id;
@@ -143,7 +147,7 @@ export class LichCongViecPage {
                 this.Detail.Ngay=element.Ngay;
             }
         });
-
+        //thay đổi trạng thái của công việc nếu chưa thiết lập thông báo thì trạng thái tắt, nếu đã thiết lập thì trạng thái bật.
         setTimeout(() => {
             if (this.TrangThai == 0) {
                 this.checkBat = false;
@@ -153,6 +157,7 @@ export class LichCongViecPage {
                 this.checkBat = true;
                 this.checkTat = false;
             }
+            //thiết lập option hiện lựa chọn
             let alert = this.alertCtrl.create();
             alert.setTitle('Lựa Chọn');
             alert.addInput({
@@ -173,20 +178,25 @@ export class LichCongViecPage {
                 handler: data => {
                     console.log('Radio data:', data);
                     console.log(data);
+                    //if chọn bật thì kiểm tra trạng thái xem nếu đang bật thì không đặt đc nữa
                     if (data == "1") {
                         if (this.TrangThai == data) {
                             this.HienThongBao("Thông Báo!", "Công việc này đã được đặt trước đó!");
                         }
+                        //nếu chưa thì đặt thông báo cho công việc đó.thì thiết lập đặt thông báo.
                         else {
                             this.TrangThai = 1;
                             this.DatThongBao();
                         }
 
                     }
+                    //nếu chọn tắt.
                     if (data == "0") {
+                        //nếu công việc chưa đc thiết lập thì hiện thông báo
                         if (this.TrangThai == data) {
                             this.HienThongBao("Thông Báo!", "Công việc này chưa được đặt thông báo!");
                         }
+                        //nếu cv đã có thiết lập thì xóa thiết lập cho cv đó.
                         else {
                             this.TrangThai = 0;
                             this.XoaTb();
@@ -206,30 +216,14 @@ export class LichCongViecPage {
 
 
     }
+    //đặt thông báo cho công việc
     DatThongBao() {
+        //tách thời gian
         this.TachThoiGian();
+        //đặt notify
         this.HienNotify();
     }
-    nhaynhay(id: number, checked: boolean) {
-        this.Lich.forEach(element => {
-            if (element.id == id) {
-                this.Detail.id = element.id;
-                this.Detail.NoiDung = element.NoiDung;
-                this.Detail.ThoiGian = element.ThoiGian;
-            }
-        });
-        if (checked == true) {
-            this.HienThongBao("Thông Báo", "Đã hẹn giờ!");
-            this.TachThoiGian();
-            this.HienNotify();
-        }
-        else {
-            this.XoaTb();
-        }
-        console.log(this.Detail.id);
-
-
-    }
+    //Hiện thông báo.
     HienThongBao(title, sub) {
         let alert = this.alertCtrl.create({
             title: title,
@@ -240,6 +234,7 @@ export class LichCongViecPage {
             alert.present();
         }, 200)
     }
+    //tách ngày của công việc
     TachNgay() {
         this.ThangCv = this.Detail.Ngay.split('/')[1];
         this.NgayCv = this.Detail.Ngay.split('/')[2];
@@ -249,6 +244,7 @@ export class LichCongViecPage {
         console.log(this.ThangCv);
 
     }
+    //tách thời gian của công việc
     TachThoiGian() {
         //lấy thời gian của công việc tách ra để cài thời gian hẹn của notify
         this.GioCv = this.Detail.ThoiGian.split(':')[0];
@@ -257,50 +253,65 @@ export class LichCongViecPage {
         console.log(this.PhutCv);
 
     }
+    //đặt notify cho công việc
     HienNotify() {
+        //lấy tgian hiện tại
         var d = new Date();
         var year = d.getFullYear();
         var month = d.getMonth();
         var day = d.getDate();
         // var hours = d.getHours();
         // var minutes = d.getMinutes();
+        //tách ngày
         this.TachNgay();
+        //tính số lần lặp
         this.SoLanLap = this.ThoiGianHen / this.ThoiGianLap;
         //thời gian báo.
+        //nếu phút của công việc < thời gian hẹn thì ta lấy giờ cv -1 và phút = 60-(thời gian hẹn - phút cv)
         if (this.PhutCv - this.ThoiGianHen < 0) {
             this.GioCv = this.GioCv - 1;
             this.PhutCv = 60 - (this.ThoiGianHen - this.PhutCv);
         }
+        //nếu phút cv > thời gian hẹn, phút cv = phút cv - thời gian hẹn
         else {
             this.PhutCv = this.PhutCv - this.ThoiGianHen;
         }
         console.log(this.GioCv + "---" + this.PhutCv);
+        //tính ngày
+        //nếu ngày cv > ngày hiện tại và tháng giống nhau thì ngày của thời gian báo notify = ngày công việc
         if (this.NgayCv > day && this.ThangCv == month) {
             day = this.NgayCv;
         }
         else {
+            //nếu ngày giống nhau mà tháng của cv > tháng hiện tại thì tháng của notify = tháng cv
             if (this.ThangCv > month) {
                 month = this.ThangCv-1;
                 day = this.NgayCv;
             }
         }
+        //thời gian hẹn của notify
         var reminder = new Date(year, month, day, this.GioCv, this.PhutCv, 0);
         console.log(reminder);
         let notifi;
+        
         this.idNotify = this.Detail.id;
+        //nếu ngày cv < ngày hiện tại  không đặt thông báo được
         if (this.NgayCv < day) {
             this.HienThongBao("Thông Báo!", "Công việc đã qua không thể đặt thông báo!")
         }
         else {
+            // nếu thời gian hẹn của notify < thời gian hiện tại hiện thông báo
             if (d > reminder) {
                 this.HienThongBao("Thông Báo!", "Công việc đã qua không thể đặt thông báo!");
             }
             else {
+                // theo số lần lặp đặt thông báo.
                 for (let i = 0; i < this.SoLanLap + 1; i++) {
+                    //nếu lặp lần thứ 2 thì ta lấy số phút notify cộng thêm 1 khoảng thời gian lặp
                     if (i > 0) {
                         this.PhutCv = (this.PhutCv + this.ThoiGianLap * 1);
                     }
-
+                    //set lại id cho notify cộng thêm 1 chuổi để id khác nhau
                     this.idNotify = this.idNotify + (15071995 * 1);
                     console.log(i);
                     console.log(year + "-" + month + "-" + day + "-" + this.GioCv + "-" + this.PhutCv);
@@ -314,8 +325,10 @@ export class LichCongViecPage {
                         at: reminder,
                         icon: "file://assets/img/clock1.png"
                     };
+                    //set notify
                     LocalNotifications.schedule(notifi);
                     console.log(notifi);
+                    //set background
                     BackgroundMode.enable();
                 }
                 this.HienThongBao("Thông Báo", "Đã hẹn giờ!");
@@ -324,16 +337,20 @@ export class LichCongViecPage {
 
 
     }
+    //xóa notify theo id của công viêc đc chọn
     XoaTb() {
         this.idNotify = this.Detail.id;
+        // lấy id đc chọn cộng thêm chuổi mặc đinh để xóa tất cả notify của công việc đó.
         for (let i = 0; i < 30; i++) {
             this.idNotify = this.idNotify + (15071995 * 1);
             console.log(this.idNotify);
+            //xóa notify
             LocalNotifications.cancel(this.idNotify).then(results => {
                 console.log("Cancel:");
                 console.log(results);
 
             });
+            //xóa notify
             LocalNotifications.clear(this.idNotify).then(results => {
                 console.log("Clear:");
                 console.log(results);
@@ -345,36 +362,9 @@ export class LichCongViecPage {
             this.HienThongBao("Thông Báo", "Đã xóa đặt giờ !");
         }, 300)
     }
-    TonTai() {
-        //kiểm tra xem công việc nào đã được đặt
-        this.Lich.forEach(element => {
-            LocalNotifications.isScheduled(element.id).then(results => {
-                if (results == true) {
-                    console.log(element.id + "-" + results);
-                    this.TrangThai = 1;
-                    this.Checked = results;
-                    console.log(this.Checked);
-                    element["TrangThai"] = this.TrangThai;
-                }
-                else {
-                    console.log(element.id + "-" + results);
-                    this.TrangThai = 0;
-                    this.Checked = results;
-                    console.log(this.Checked);
-                    element["TrangThai"] = this.TrangThai;
-                }
-            }).catch(results => {
-                console.log(results);
-                this.Checked = false;
-            })
-        });
-        setTimeout(() => {
-            console.log(this.Lich);
-        }, 1000)
-
-    }
+    //kiểm tra công việc xem đã đặt thông báo chưa theo id 
     KiemTra(id) {
-        //kiểm tra công việc nào đã được đặt theo id công việc
+        //kiểm tra công việc nào đã được đặt theo id công việc, chuyển trạng thái lại =1 nếu đã đc đặt =0 nếu chưa
         LocalNotifications.isScheduled(id).then(results => {
             if (results == true) {
                 console.log(id + "-" + results);
